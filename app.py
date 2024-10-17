2ea22ecb16a537977ea9e5b08bf4bcaa4dc0b6b4
Lucas Teasdale <c0907924@mylambton.ca>
2024-10-16 20:51:39 -0400

    changed url

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-from flask import Flask
-from dotenv import load_dotenv
 import os
+from flask import Flask
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-load_dotenv()  
+load_dotenv()
 
-app = Flask(__name__)
 
 MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
 MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
-MONGODB_CLUSTER_URL = os.getenv('MONGODB_CLUSTER_URL')
+MONGODB_CLUSTER_URL = os.getenv('MONGODB_CLUSTER_URL')  
 
-from pymongo import MongoClient
 connection_string = f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@{MONGODB_CLUSTER_URL}/shop_db?retryWrites=true&w=majority"
+
 client = MongoClient(connection_string)
 db = client.shop_db
 products_collection = db.products
 
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "Welcome to the E-Commerce Store!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
